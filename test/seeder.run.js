c5282a57a8ef62b9f2b3e01b7ae11c60da57a293
Pat Cavit <pcavit@gmail.com>
2014-02-24 07:53:08 -0800

    Standardize errors passed to done callback
    Also started filling out Seeder.run() tests

--- test/seeder.run.js
+++ test/seeder.run.js
@@ -7,10 +7,43 @@ var path   = require("path"),
     Seeder = require("../lib/seeder");
 
 describe("yui-seeder", function() {
-    describe("Seeder Class", function() {
-        it("should throw when no callback is provided");
-        it("should provide an error if a config can't be found");
-        it("should provide an error if no modules would be combined");
+    describe.only("Seeder Class", function() {
+        it("should throw when no callback is provided", function() {
+            var s = new Seeder();
+
+            assert.throws(function() {
+                s.run()
+            });
+        });
+
+        it("should provide an error if a config can't be found", function(done) {
+            var s = new Seeder({
+                    configs : [ "fooga.js" ]
+                });
+
+            s.run(function(err) {
+                assert(err);
+                assert(err.toString().indexOf("Cannot find module") > -1);
+
+                done();
+            });
+        });
+
+        it("should provide an error if no modules would be combined", function(done) {
+            var s = new Seeder({
+                    cwd     : "./test/specimens/simple",
+                    configs : [ "_config.js" ],
+                    modules : [ "fooga" ]
+                });
+
+            s.run(function(err) {
+                assert(err);
+                assert(err.toString().indexOf("No modules to concat") > -1);
+
+                done();
+            });
+        });
+
         it("should provide an error if a module can't be found");
 
         it("should call done with no args on success");
