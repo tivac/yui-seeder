da9ba929908b31516d7e3fb4cd78c347191bd96f
Pat Cavit <pcavit@gmail.com>
2014-02-23 23:38:44 -0800

    Work on supporting multiple configs
    & modules, though that's simpler

--- args.js
+++ args.js
@@ -2,28 +2,29 @@
 "use strict";
 
 module.exports = {
-    module : {
+    modules : {
         position : 0,
-        help     : "YUI alias/module to use for module merging",
-        required : true
+        help     : "YUI aliases/modules to use for generation",
+        required : true,
+        list     : true
     },
 
     configs : {
         string   : "-c CONFIG --configs=CONFIG",
-        help     : "Config file paths to resolve modules dependencies",
+        help     : "Config file paths",
         list     : true,
         required : true
     },
 
     output : {
         string  : "-o FILE   --output=FILE",
-        help    : "File to write the merged modules to",
+        help    : "File to write merged modules to",
         default : "stdout"
     },
 
     append : {
         string  : "-a,       --append",
-        help    : "Append merged modules instead of overwriting",
+        help    : "Append output instead of overwriting",
         flag    : true
     }
 };
