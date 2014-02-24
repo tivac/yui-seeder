118a11de777008e8296f950d46554d7c9bc255d6
Pat Cavit <pcavit@gmail.com>
2014-02-24 14:33:18 -0800

    Break apart CLI to (hopefully) ease testing

--- bin/cli.js
+++ bin/cli.js
@@ -1,28 +1,7 @@
 /*jshint node:true */
 "use strict";
 
-var Seeder = require("../lib/seeder"),
-    argv   = require("nomnom")
-        .script("seeder")
-        .options(require("../args.js"))
-        .parse(),
+var cli    = require("../lib/cli"),
+    seeder = require("../lib/seeder");
 
-    seeder;
-
-seeder = new Seeder(argv);
-
-seeder.run(function(err, data) {
-    if(err) {
-        console.error(err);
-
-        return process.on("exit", function() {
-            process.exit(1);
-        });
-    }
-
-    if(data) {
-        // process.stdout instad of console.log to avoid extra
-        // trailing newlines
-        process.stdout.write(data);
-    }
-});
+cli(process, seeder);
