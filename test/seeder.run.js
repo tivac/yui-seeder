fd1e09c98b4648acd61465a9590352788214b8f6
Pat Cavit <pcavit@gmail.com>
2014-02-24 10:55:37 -0800

    Fill out Seeder.run() tests

--- test/seeder.run.js
+++ test/seeder.run.js
@@ -1,18 +1,32 @@
 /*jshint node:true */
 "use strict";
 
-var path   = require("path"),
+var fs     = require("fs"),
+    path   = require("path"),
     assert = require("assert"),
 
-    Seeder = require("../lib/seeder");
+    Seeder = require("../lib/seeder"),
+    temp   = path.join(__dirname, "/temp.js");
 
 describe("yui-seeder", function() {
-    describe.only("Seeder Class", function() {
+    describe("Seeder Class", function() {
+        afterEach(function(done) {
+            if(fs.existsSync(temp)) {
+                return fs.unlink(temp, done);
+            }
+
+            done();
+        });
+
         it("should throw when no callback is provided", function() {
             var s = new Seeder();
+            debugger;
+            assert.throws(function() {
+                s.run();
+            });
 
             assert.throws(function() {
-                s.run()
+                s.run("a");
             });
         });
 
@@ -44,11 +58,116 @@ describe("yui-seeder", function() {
             });
         });
 
-        it("should provide an error if a module can't be found");
+        it("should call done with no args on success", function(done) {
+            var s = new Seeder({
+                cwd     : "./test/specimens/simple",
+                configs : [ "_config.js" ],
+                modules : [ "a" ]
+            });
 
-        it("should call done with no args on success");
-        it("should write out a file containing combined modules");
-        it("should overwrite existing contents by default");
-        it("should append to existing contents if requested");
+            s.run(function(err) {
+                assert.ifError(err);
+
+                done();
+            });
+        });
+
+        it("should pass the combined modules to done", function(done) {
+            var s = new Seeder({
+                cwd     : "./test/specimens/simple",
+                configs : [ "_config.js" ],
+                modules : [ "a" ]
+            });
+
+            s.run(function(err, data) {
+                assert.ifError(err);
+                
+                assert(data);
+                assert(data.indexOf("YUI.add(\"a") > -1);
+                assert(data.indexOf("YUI.add(\"b") > -1);
+                assert(data.indexOf("YUI.add(\"c") > -1);
+
+                done();
+            });
+        });
+
+        it("should write the combined modules to a file", function(done) {
+            var s = new Seeder({
+                cwd     : "./test/specimens/simple",
+                configs : [ "_config.js" ],
+                modules : [ "a" ],
+                output  : temp
+            });
+
+            s.run(function(err) {
+                var result;
+
+                assert.ifError(err);
+                
+                result = fs.readFileSync(temp, "utf8");
+
+                assert(result);
+                assert(result.indexOf("YUI.add(\"a") > -1);
+                assert(result.indexOf("YUI.add(\"b") > -1);
+                assert(result.indexOf("YUI.add(\"c") > -1);
+
+                done();
+            });
+        });
+
+        it("should write the combined modules to a file", function(done) {
+            var s = new Seeder({
+                cwd     : "./test/specimens/simple",
+                configs : [ "_config.js" ],
+                modules : [ "a" ],
+                output  : temp
+            });
+
+            fs.writeFileSync(temp, "fooga booga");
+
+            s.run(function(err) {
+                var result;
+
+                assert.ifError(err);
+                
+                result = fs.readFileSync(temp, "utf8");
+
+                assert(result);
+                assert(result.indexOf("fooga booga") === -1);
+                assert(result.indexOf("YUI.add(\"a") > -1);
+                assert(result.indexOf("YUI.add(\"b") > -1);
+                assert(result.indexOf("YUI.add(\"c") > -1);
+
+                done();
+            });
+        });
+
+        it("should append to existing contents if requested", function(done) {
+            var s = new Seeder({
+                cwd     : "./test/specimens/simple",
+                configs : [ "_config.js" ],
+                modules : [ "a" ],
+                output  : temp,
+                append  : true
+            });
+
+            fs.writeFileSync(temp, "fooga booga");
+
+            s.run(function(err) {
+                var result;
+
+                assert.ifError(err);
+                
+                result = fs.readFileSync(temp, "utf8");
+
+                assert(result);
+                assert(result.indexOf("fooga booga") > -1);
+                assert(result.indexOf("YUI.add(\"a") > -1);
+                assert(result.indexOf("YUI.add(\"b") > -1);
+                assert(result.indexOf("YUI.add(\"c") > -1);
+
+                done();
+            });
+        });
     });
 });
