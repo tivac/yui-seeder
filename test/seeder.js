11edbe970f71c82446b93c4ab0fba2be150a0f88
Pat Cavit <pcavit@gmail.com>
2014-02-23 23:40:04 -0800

    Fleshing out tests

--- test/seeder.js
+++ test/seeder.js
@@ -8,27 +8,6 @@ var path   = require("path"),
 
 describe("yui-seeder", function() {
     describe("Seeder Class", function() {
-        describe("_loadConfig()", function() {
-            it("should find a config using globs", function(){
-                var s = new Seeder({
-                        cwd    : "./test/specimens/simple",
-                        config : "**/_config.js"
-                    }),
-                    config = s._loadConfig();
-
-                assert(config.length);
-                assert.equal(config, path.normalize("test/specimens/simple/_config.js"));
-            });
-
-            it("should return false when a config can't be found", function() {
-                var s = new Seeder({
-                        cwd    : "./test/specimens/simple",
-                        config : "**/_fooga.js"
-                    }),
-                    config = s._loadConfig();
-
-                assert.equal(config, false);
-            });
-        });
+        it("should ")
     });
 });
